0b38826874e588b8e38c1e6fc83377fa5b77d3f2
A.S. Lomoff <lomov.as@gmail.com>
2014-03-21 19:56:17 +0300

    add azure cli

--- cookbooks/juju-dev-box/recipes/finalize.rb
+++ cookbooks/juju-dev-box/recipes/finalize.rb
@@ -4,3 +4,11 @@ execute "use skel ubuntu colors and aliases" do
     cp /etc/skel/.bashrc ~/.bashrc
   BASH
 end
+
+execute "install azure command line tools" do
+  # due to an oppen bug: https://bugs.launchpad.net/juju/+bug/998238
+  command <<-BASH
+    apt-get install npm && npm install azure-cli -g
+  BASH
+end
+
